98956f58fea85903be3f197a80d48b2d495e381d
Edward Dupre <edwd42@gmail.com>
2017-10-01 01:49:28 +0000

    initial commit

---
+++ spec/test.spec.js
@@ -0,0 +1,27 @@
+class Calculator{
+    add(a,b){
+        return a + b;
+    } 
+    minus(a,b){
+        return a - b;
+    }
+}
+
+describe('calculate addition', function(){
+    var calculate = new Calculator();
+    
+    it('should be able to add two numbers together', function(){
+        expect(calculate.add(1,3).toBe(5));
+        console.log('I was able to add two numbers');
+    });
+});
+
+    describe('calculate addition', function(){
+        it('should be able to subtract two numbers', function(){
+            expect(calculate.minus(1,3).toBe(5));
+            console.log('I was able to add two numbers');
+        });
+    });
+
+
+
